388559512506acb2dd924d7f8894ef5185829747
Aakash Kalmady <arkalmady@gmail.com>
2025-06-28 19:04:47 -0400

    add mock navbar

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,16 +1,36 @@
 import { SignedIn, UserButton } from "@clerk/nextjs";
 import { currentUser } from "@clerk/nextjs/server";
+import Link from "next/link";
+import Image from "next/image";
 
 export default async function Page() {
   const user = await currentUser();
   return (
     <div>
-      <SignedIn>
-        <UserButton />
-      </SignedIn>
+      <nav className="flex items-center p-4 w-full bg-gray-500">
+        <div>
+          <Link
+            href="/dashboard"
+            className="flex items-center gap-1 hover:scale-150 duration-500 "
+          >
+            <Image
+              src="/assets/logo.svg"
+              width={60}
+              height={60}
+              alt="Let's talk"
+            />
+          </Link>
+        </div>
+        <div className="ml-auto">
+          <SignedIn>
+            <UserButton />
+          </SignedIn>
+        </div>
+      </nav>
       <div>
-        <p>Hi {user?.firstName}!</p>
+        <p>Hi {user?.fullName}!</p>
         <p>Welcome to ClearVue</p>
+        <p>Your Dashboard</p>
       </div>
     </div>
   );
