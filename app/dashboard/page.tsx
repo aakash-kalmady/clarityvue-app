7ed1f0a0b7d5a1c5ce2bc0f6680431a480182ec2
Aakash Kalmady <arkalmady@gmail.com>
2025-06-29 13:23:17 -0400

    add new logo, add navbar component

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -2,36 +2,16 @@ import { SignedIn, UserButton } from "@clerk/nextjs";
 import { currentUser } from "@clerk/nextjs/server";
 import Link from "next/link";
 import Image from "next/image";
+import NavBar from "@/components/NavBar";
 
 export default async function Page() {
   const user = await currentUser();
   return (
     <div>
-      <nav className="flex items-center p-4 w-full bg-gray-500">
-        <div>
-          <Link
-            href="/dashboard"
-            className="flex items-center gap-1 hover:scale-150 duration-500 "
-          >
-            <Image
-              src="/assets/logo.svg"
-              width={60}
-              height={60}
-              alt="Let's talk"
-            />
-          </Link>
-        </div>
-        <div className="ml-auto">
-          <SignedIn>
-            <UserButton />
-          </SignedIn>
-        </div>
-      </nav>
-      <div>
-        <p>Hi {user?.fullName}!</p>
-        <p>Welcome to ClearVue</p>
-        <p>Your Dashboard</p>
-      </div>
+      <NavBar />
+      <p>Hi {user?.fullName}!</p>
+      <p>Welcome to ClearVue</p>
+      <p>Your Dashboard</p>
     </div>
   );
 }
--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -1,3 +1,28 @@
+import Link from "next/link";
+import Image from "next/image";
+import { SignedIn, UserButton } from "@clerk/nextjs";
+
 export default function NavBar() {
-  return <div>hi</div>;
+  return (
+    <nav className="flex items-center p-4 w-full bg-gray-500">
+      <div>
+        <Link
+          href="/dashboard"
+          className="flex items-center gap-1 hover:scale-150 duration-500 "
+        >
+          <Image
+            src="/assets/logo.png"
+            width={100}
+            height={100}
+            alt="Let's talk"
+          />
+        </Link>
+      </div>
+      <div className="ml-auto">
+        <SignedIn>
+          <UserButton />
+        </SignedIn>
+      </div>
+    </nav>
+  );
 }
