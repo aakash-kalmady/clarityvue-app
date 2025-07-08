4e5fb3cb34dce425709d75d2f597309320e9bbc2
Aakash Kalmady <arkalmady@gmail.com>
2025-07-08 14:35:20 -0400

    add privacy policy to home page

--- components/LandingPage.tsx
+++ components/LandingPage.tsx
@@ -1,9 +1,11 @@
 import { SignIn } from "@clerk/nextjs";
 import Image from "next/image";
+import Link from "next/link";
+import { Button } from "./ui/button";
 
 export default function LandingPage() {
   return (
-    <main className="w-full h-screen bg-zinc-900 flex items-center justify-center gap-10 p-5 max-md:flex-col ">
+    <main className="relative w-full min-h-screen bg-zinc-900 flex items-center justify-center gap-10 p-5 max-md:flex-col ">
       <section className="flex flex-col items-center justify-center max-sm:flex-row">
         <div>
           <h1 className="text-2xl font-black lg:text-3xl text-white">
@@ -19,6 +21,12 @@ export default function LandingPage() {
       </section>
       <div className="">
         <SignIn routing="hash" />
+      </div>
+      <div className="absolute text-xs p-4 bottom-0 bg-neutral-900 left-0 right-0 h-15 flex items-center justify-around">
+        <p>&copy; AAKASH KALMADY 2025.</p>
+        <Button variant="link" asChild size="sm">
+          <Link href="/privacy">Our Privacy Policy</Link>
+        </Button>
       </div>
     </main>
   );
