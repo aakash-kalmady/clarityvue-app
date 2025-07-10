04ea4fa7555fab55744e001c5deccf123f8aab2e
Aakash Kalmady <arkalmady@gmail.com>
2025-07-10 18:16:37 -0400

    debug build problem

--- next.config.ts
+++ next.config.ts
@@ -8,16 +8,8 @@ const nextConfig: NextConfig = {
       new URL("https://clarityvue.s3.us-east-2.amazonaws.com/**"),
       new URL("https://www.shutterstock.com/**"),
     ],
-    formats: ["image/webp", "image/avif"],
-    minimumCacheTTL: 60 * 60 * 24 * 30, // 30 days
   },
   devIndicators: false,
-  experimental: {
-    optimizeCss: true,
-  },
-  compiler: {
-    removeConsole: process.env.NODE_ENV === "production",
-  },
 };
 
 export default nextConfig;
